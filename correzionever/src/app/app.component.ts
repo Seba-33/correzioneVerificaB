bee8834cecaf979da289b7136d1c2981d06ca9ba
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2025-05-21 16:43:57 +0000

    ottavo punto

--- correzionever/src/app/app.component.ts
+++ correzionever/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { CartProduct } from './Models/cartproduct.model';
 import { DetailsComponent } from './details/details.component';
+import { CarrelloComponent } from './carrello/carrello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,VegetableListComponent,DetailsComponent],
+  imports: [RouterOutlet,VegetableListComponent,DetailsComponent,CarrelloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
